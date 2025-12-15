84f03001db05282a0024dc3ffea208ce48f0f2c4
Devender01 <sdeven098@gmail.com>
2025-12-15 17:57:26 +0530

    Updated code

--- login.js
+++ login.js
@@ -7,7 +7,37 @@ function login() {
         return;
     }
 
-    const uniqueID = email.substring(1, 5);
+   
+    //  ID = Dev123- +everythingbefore @
+   
+    const usernamePart = email.split("@")[0];  
+    const uniqueID = "Dev123" + usernamePart;
+
+    
+    // Extract First + Last name
+    
+    const parts = usernamePart.split(".");
+    const firstName = parts[0] || "User";
+    const lastName = parts.length > 1 ? parts.slice(1).join(" ") : parts[0];
+
+    
+    //   Identify call
+   
+    aptrinsic("identify",
+        {
+            id: uniqueID,     
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+     Plan: "gold",
+            price: 95.5
+        },
+        {
+            id: "salesforce",
+            name: "Salesforce Solutions",
+            Program: "Premium"
+        }
+    );
 
     alert("Successful Login!\nUnique ID: " + uniqueID);
     window.location.href = "one.html";
--- thankyou.js
+++ thankyou.js
@@ -1,6 +1,12 @@
+
 function goLogout() {
+  
+    aptrinsic('reset');
+
+    
     window.location.href = 'index.html';
 }
+
 
 function buttonClicked() {
     alert("Button clicked");
